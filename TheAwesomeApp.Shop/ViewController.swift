ad27312b6aec599499edf7f59a5865d91ba6c5de
Pedro Curich <curichpedro@gmail.com>
2020-05-08 05:03:25 -0500

    first commit

---
+++ TheAwesomeApp.Shop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheAwesomeApp.Shop
+//
+//  Created by QUBICGO SAC on 5/8/20.
+//  Copyright © 2020 Qubicgo SAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
